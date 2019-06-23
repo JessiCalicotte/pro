93f7e02de202026f72cf5a00ab5c951d2a0a168a
Jessi Calicotte <orgkleen@gmail.com>
2019-06-23 18:55:02 +0000

    bootstap modal for the quotes form

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,9 +3,7 @@ class QuotesController < ApplicationController
      @quote = Quote.order("RANDOM()").first
   end
 
-   def new
-    @quote = Quote.new
-  end
+  
   def create
       @quote = Quote.create(quote_params)
   if @quote.invalid?
@@ -13,13 +11,10 @@ class QuotesController < ApplicationController
   end
     redirect_to root_path
   end
-  
+
     def about
   end
 
-
-
-  private
 
   def quote_params
     params.require(:quote).permit(:saying, :author)
